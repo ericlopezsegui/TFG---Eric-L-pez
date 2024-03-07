d5d240024630188e4b89291e6bbedb66013e4ece
Eric López <lopezs.eric@gmail.com>
2024-03-07 12:43:16 +0100

    adding validations and error handlers

--- project/server/app.py
+++ project/server/app.py
@@ -1,13 +1,31 @@
+from http.client import HTTPException
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from db_config import Config, Testing, Development, Production
 
 db = SQLAlchemy()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/Database_torneig'
+app.config.from_object(Development)
 
 # Inicialización de la extensión SQLAlchemy con la aplicación
 db.init_app(app)
+
+class validationError (HTTPException):
+    code = 400
+    description = 'Validation Error'
+
+@app.errorhandler(validationError)
+def handleValidationError(e):
+    response = jsonify({'eroor': e.Description})
+    response.status_code = e.code
+    return response
+
+@app.errorhandler(Exception)
+def unexpectedError (error):
+    response = jsonify({'message':'An unexpected error occurred {}'.format(error)})
+    response.status_code = 500
+    return response
 
 from routes import *
 
